992230cb5ff1d986597305189ff34bd44f0f5969
yangyang <yangjoanne216@gmail.com>
2026-05-06 13:26:56 +0200

    add introduction page and create commitment page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'create',
+    loadComponent: () => import('./pages/create/create').then((m) => m.CreateComponent),
+  },
+  {
+    path: 'review',
+    loadComponent: () => import('./pages/review/review').then((m) => m.ReviewComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
